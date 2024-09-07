1227bb2d85adef7d2a3f6dbc6d720ba723133b57
Pankaj D <94873315+xvixivxix@users.noreply.github.com>
2024-09-07 10:14:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("3+3") // we are using nodejs rather than some browser console that comes with js engine
+
+console.log(3+3);
+
+console.log("Pankaj")
+
+let name = "pankaj"
+let age = 23
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "pankaj");
+console.log(typeof age);
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
