6b45e0e6828393a8b8e03f9b91850fa4507147c2
Pankaj D <94873315+xvixivxix@users.noreply.github.com>
2024-09-08 07:24:24 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,27 +1,72 @@
-let score = "pankaj"
+// let score = "pankaj"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNubmer);
+/* let valueInNumber = Number(score)
+console.log(typeof valueInNubmer); */
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "pankaj"
+/* let isLoggedIn = "pankaj"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 console.log(booleanIsLoggedIn);
+*/
 
 // 1 => true; 0 => false
 // "" => false
 // "pankaj" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// *************** Operations ***************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+//console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Pankaj"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3) // use paranthesis rather than writing messey code.
+ 
+// if string is first then everything is conversted to string and if number is first then operation is performed on number then string is executed
+
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
