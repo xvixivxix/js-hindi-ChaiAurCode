1ac6d7b2fcf9fd891afa1cbbf8c425ffa5dc2739
Pankaj D <94873315+xvixivxix@users.noreply.github.com>
2024-09-08 08:24:45 +0000

    memory concept

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -34,3 +34,29 @@ const myFunction = function(){
 }
 
 console.log(typeof bigNumber);
+
+
+// +++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "pankajddubeydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "pankaj@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
